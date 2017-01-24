d900dc8fd11b398dc1cc5dfaa72052dc2c04b71f
Sam Frier <sfrier@qa.com>
2017-01-24 16:07:00 +0000

    commit-msg hook

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+set -euxo pipefail
 
-echo "Hello world"
+echo "Setting up Git hooks..."
+ln commit-msg .git/hooks/commit-msg
+chmod +x .git/hooks/commit-msg
+echo "All done!"
+
+exit 0
