761410136a742f7e9fb78bf0873a9e9a8c511ddf
Sam Frier <sfrier@qa.com>
2017-01-25 12:13:02 +0000

    Allow user to specify project when running setup

--- setup.sh
+++ setup.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-set -euxo pipefail
+# set -euxo pipefail
 
-echo "Setting up Git hooks..."
-ln commit-msg .git/hooks/prepare-commit-msg
-ln commit-msg .git/hooks/commit-msg
+echo "Reading project key..."
+if [ $# -ne 1 ]
+then
+  echo "Error: please supply exactly one argument representing the Jira project key."
+  exit 1
+else
+  project_key=$1
+fi
+
+echo "Substituting project key..."
+sed -i 's/\[PROJECT_KEY\]/'$project_key'/g' commit-msg
+echo "Copying Git hooks..."
+cp prepare-commit-msg .git/hooks/
+cp commit-msg .git/hooks/
 echo "All done!"
 
 exit 0
