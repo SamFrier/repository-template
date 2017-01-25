9fb2ae4dcd2eedd29600a5d24d9bb5749ae40f26
Sam Frier <sfrier@qa.com>
2017-01-25 11:01:17 +0000

    Added prepare-commit-msg hook

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 set -euxo pipefail
 
 echo "Setting up Git hooks..."
+ln commit-msg .git/hooks/prepare-commit-msg
 ln commit-msg .git/hooks/commit-msg
-chmod +x .git/hooks/commit-msg
 echo "All done!"
 
 exit 0
